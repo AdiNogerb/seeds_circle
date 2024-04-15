5f3adff0c37f3b1ccf239ab3a947cd1db5e3d199
christopher <christopher80090@hotmail.fr>
2024-04-15 14:51:44 +0200

    image map home

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,17 @@
 // Variable pour URL de requête API
 let url = 'https://opendata.agencebio.org/api/gouv/operateurs/?activite=Production&produit=Graines&nb=100';
+let map = document.getElementById('map');
+let spinner = document.getElementById('spinner');
+let greenIcon = L.icon({
+    iconUrl: 'public/assets/img/leaf-green.png',
+    shadowUrl: 'public/assets/img/leaf-shadow.png',
 
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 // Fonction pour rechercher sur l'API les producteurs de graines dans un rayon de 200km
 getSellers = (lat, lng) => {
@@ -35,8 +46,10 @@ getSellers = (lat, lng) => {
             let adresseObj = adresseOperateur[0];
             let latSell = adresseObj.lat;
             let lngSell = adresseObj.long;
-            var marker = L.marker([latSell, lngSell]).addTo(map);
+            var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
         });
+
+        spinner.classList.add('d-none');
     })
 }
 
