18289d7c7802b20e7300b9775281b37fd012afb2
adrienbregon <adrien.bregon@gmail.com>
2024-04-16 14:35:24 +0200

    maj script location user

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -13,8 +13,24 @@ const greenIcon = L.icon({
 // Variable pour URL de requête API
 let url = 'https://opendata.agencebio.org/api/gouv/operateurs/?activite=Production&produit=Graines&nb=100';
 
+
 // Fonction pour rechercher sur l'API les producteurs de graines dans un rayon de 200km
 getSellers = (lat, lng) => {
+    console.log(lat, lng);
+
+    // Condition : si l'utilisateur s'est déplacé alors supprimer le tableau datas du localStorage
+    if (localStorage.getItem('position')) {
+        let position = JSON.parse(localStorage.getItem('position'));
+        if (Math.abs(lat - position[0]) > 0.1 || Math.abs(lng - position[1]) > 0.1) {
+            localStorage.removeItem('datas');
+            let position = [lat, lng];
+            localStorage.setItem('position', JSON.stringify(position));    
+        }
+    } else {
+        let position = [lat, lng];
+        localStorage.setItem('position', JSON.stringify(position));
+    }
+
     // Spécification de la latitude et longitude de l'opérateur dans la requête URL
     url += `&lat=${lat}`;
     url += `&lng=${lng}`;
@@ -31,47 +47,54 @@ getSellers = (lat, lng) => {
     // Placement de la première tuile sur le lieu de l'utilisateur
     var marker = L.marker([lat, lng]).addTo(map);
 
+    // Fonction pour les taches à effectuer sur les items du tableau suivant
+    const itemList = (item) => {
+        let adresseOperateur = item.adressesOperateurs;
+        let name = item.denominationcourante;
+        if (name == null) {
+            name = '';
+        }
+        let mail = item.email;
+        let adresseObj = adresseOperateur[0];
+        let site = item.siteWebs;
+        let latSell = adresseObj.lat;
+        let lngSell = adresseObj.long;
+        let place = adresseObj.lieu;
+        let postal = adresseObj.codePostal;
+        let city = adresseObj.ville;
+        if (mail != null && site.length != 0) {
+            let siteObj = site[0];
+            let siteUrl = siteObj.url;                
+            var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
+            <p class="fw-bold">${name}</p>
+            <p>${place}, ${postal}, ${city}</p>
+            <p><a href="mailto: ${mail}">${mail}</a></p>
+            <a href="${siteUrl}" target="_blank">${siteUrl}</a>`);
+        } else if (mail != null && site.length == 0){
+            var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
+            <p class="fw-bold">${name}</p>
+            <p>${place}, ${postal}, ${city}</p>
+            <a href="mailto: ${mail}">${mail}</a>`);
+        } else if (mail == null && site.length != 0){
+            let siteObj = site[0];
+            let siteUrl = siteObj.url;                
+            var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
+            <p class="fw-bold">${name}</p>
+            <p>${place}, ${postal}, ${city}</p>
+            <a href="${siteUrl}" target="_blank">${siteUrl}</a>`);
+        } else {
+            var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
+            <p class="fw-bold">${name}</p>
+            <p>${place}, ${postal}, ${city}</p>`);
+        }
+    }    
+
+    // Condition : si le localStorage contient déjà les données, ne pas executer de requête URL
     if (localStorage.getItem('datas')) {
         datas = JSON.parse(localStorage.getItem('datas'));
-        datas.forEach(item => {
-            let adresseOperateur = item.adressesOperateurs;
-            let name = item.denominationcourante;
-            if (name == null) {
-                name = '';
-            }
-            let mail = item.email;
-            let adresseObj = adresseOperateur[0];
-            let site = item.siteWebs;
-            let latSell = adresseObj.lat;
-            let lngSell = adresseObj.long;
-            let place = adresseObj.lieu;
-            let postal = adresseObj.codePostal;
-            let city = adresseObj.ville;
-            if (mail != null && site.length != 0) {
-                let siteObj = site[0];
-                let siteUrl = siteObj.url;                
-                var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
-                <p class="fw-bold">${name}</p>
-                <p>${place}, ${postal}, ${city}</p>
-                <p><a href="mailto: ${mail}">${mail}</a></p>
-                <a href="${siteUrl}" target="_blank">${siteUrl}</a>`);
-            } else if (mail != null && site.length == 0){
-                var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
-                <p class="fw-bold">${name}</p>
-                <p>${place}, ${postal}, ${city}</p>
-                <a href="mailto: ${mail}">${mail}</a>`);
-            } else if (mail == null && site.length != 0){
-                let siteObj = site[0];
-                let siteUrl = siteObj.url;                
-                var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
-                <p class="fw-bold">${name}</p>
-                <p>${place}, ${postal}, ${city}</p>
-                <a href="${siteUrl}" target="_blank">${siteUrl}</a>`);
-            } else {
-                var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
-                <p class="fw-bold">${name}</p>
-                <p>${place}, ${postal}, ${city}</p>`);
-            }
+        // Boucle sur le tableau d'objet pour récupérer latitude et longitude des commercants
+            datas.forEach(item => {
+                itemList(item);
         });
         spinner.classList.add('d-none');
     } else {
@@ -81,49 +104,10 @@ getSellers = (lat, lng) => {
             return response.json();
         })
         .then(datas =>{
-            // Console.log pour voir le tableau d'objet récupéré
-            console.log(datas.items);
-            localStorage.setItem('datas', JSON.stringify(datas.items));
             // Boucle sur le tableau d'objet pour récupérer latitude et longitude des commercants
+            localStorage.setItem('datas', JSON.stringify(datas.items));
             datas.items.forEach(item => {
-                let adresseOperateur = item.adressesOperateurs;
-                let name = item.denominationcourante;
-                if (name == null) {
-                    name = '';
-                }
-                let mail = item.email;
-                let adresseObj = adresseOperateur[0];
-                let site = item.siteWebs;
-                let latSell = adresseObj.lat;
-                let lngSell = adresseObj.long;
-                let place = adresseObj.lieu;
-                let postal = adresseObj.codePostal;
-                let city = adresseObj.ville;
-                if (mail != null && site.length != 0) {
-                    let siteObj = site[0];
-                    let siteUrl = siteObj.url;                
-                    var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
-                    <p class="fw-bold">${name}</p>
-                    <p>${place}, ${postal}, ${city}</p>
-                    <p><a href="mailto: ${mail}">${mail}</a></p>
-                    <a href="${siteUrl}" target="_blank">${siteUrl}</a>`);
-                } else if (mail != null && site.length == 0){
-                    var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
-                    <p class="fw-bold">${name}</p>
-                    <p>${place}, ${postal}, ${city}</p>
-                    <a href="mailto: ${mail}">${mail}</a>`);
-                } else if (mail == null && site.length != 0){
-                    let siteObj = site[0];
-                    let siteUrl = siteObj.url;                
-                    var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
-                    <p class="fw-bold">${name}</p>
-                    <p>${place}, ${postal}, ${city}</p>
-                    <a href="${siteUrl}" target="_blank">${siteUrl}</a>`);
-                } else {
-                    var marker = L.marker([latSell, lngSell], {icon: greenIcon}).addTo(map).bindPopup(`
-                    <p class="fw-bold">${name}</p>
-                    <p>${place}, ${postal}, ${city}</p>`);
-                }
+                itemList(item);
             });
             spinner.classList.add('d-none');
         })
